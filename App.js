c07b4a8234f1f500de99811f97cdf01f130d56d0
adun <adun@users.noreply.github.com>
2021-08-22 00:44:43 +0200

    Error while updating property 'line'cap of a view managed by: AIRMapPolyline

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import TestScreen from "./TestScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <TestScreen />
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ TestScreen.js
@@ -0,0 +1,67 @@
+import { Dimensions, StyleSheet } from "react-native";
+import React, { useEffect, useRef } from "react";
+import MapView from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+
+const TestScreen = () => {
+  const { width, height } = Dimensions.get("window");
+  const ASPECT_RATIO = width / height;
+  const LATITUDE = 37.771707;
+  const LONGITUDE = -122.4053769;
+  const LATITUDE_DELTA = 0.0922;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+  const ORIGIN = { latitude: 37.3317876, longitude: -122.0054812 };
+  const DESTINATION = { latitude: 37.771707, longitude: -122.4053769 };
+  const RESULT = [ORIGIN, DESTINATION];
+
+  const GOOGLE_MAPS_APIKEY = "...";
+
+  const mapRef = useRef(null);
+  useEffect(() => {
+    mapRef.current.fitToCoordinates(RESULT, {
+      edgePadding: {
+        right: width / 20,
+        bottom: height / 20,
+        left: width / 20,
+        top: height / 20,
+      },
+    });
+  }, []);
+
+  return (
+    <MapView
+      ref={mapRef}
+      initialRegion={{
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }}
+      style={StyleSheet.absoluteFill}
+    >
+      <MapViewDirections
+        origin={ORIGIN}
+        destination={DESTINATION}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={3}
+        strokeColor="hotpink"
+        optimizeWaypoints={true}
+        onStart={(params) => {
+          console.log(
+            `Started routing between "${params.origin}" and "${params.destination}"`
+          );
+        }}
+        onReady={(result) => {
+          console.log(`Distance: ${result.distance} km`);
+          console.log(`Duration: ${result.duration} min.`);
+        }}
+        onError={(errorMessage) => {
+          console.log(`GOT AN ERROR ${errorMessage}`);
+        }}
+      />
+    </MapView>
+  );
+};
+
+export default TestScreen;
